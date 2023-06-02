d6be39da198f6b47cb8a397f4ab22ad98e35a5f7
akl7777777 <akl7777777@163.com>
2023-06-02 14:57:00 +0800

    修复正式版不可用的问题,直接用正式版就好

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ var orai = require('./orai.js');
 var sc2e = require('./sc2e.js');
 var cha = require('./cha.js');
 var notag = require('./notag.js');
+var space = require('./space.js');
 var d8888 = require('./8888.js');
 var gamma = require('./gamma.js');
 var delta = require('./delta.js');
@@ -77,10 +78,13 @@ function translate(query, completion) {
                     chatResult = await of.translate(query, source_lang, target_lang, translate_text, completion)
                 } else if (server === 'default-back2') {
                     chatResult = await d8888.translate(query, source_lang, target_lang, translate_text, completion)
+                } else if (server === 'default-back3') {
+                    // 限流了,每分钟3次
+                    chatResult = await notag.translate(query, source_lang, target_lang, translate_text, completion)
                 } else if (server === 'oversea-1') {
                     chatResult = await cha.translate(query, source_lang, target_lang, translate_text, completion)
                 } else {
-                    chatResult = await notag.translate(query, source_lang, target_lang, translate_text, completion)
+                    chatResult = await space.translate(query, source_lang, target_lang, translate_text, completion)
                 }
                 let mode = $option.mode;
                 const configValue = readFile();
