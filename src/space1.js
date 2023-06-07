80184d6525ca452a87dc2fb8bf337736714b4aa0
akl7777777 <akl7777777@163.com>
2023-06-07 21:30:00 +0800

    修复线路

--- src/space1.js
+++ src/space1.js
@@ -90,7 +90,7 @@ async function digestMessage(r) {
 
 async function generateSignature(r) {
     const {t: e, m: t} = r;
-    const n = 'bad';
+    const n = 'do_not_do_this';
     const a = `${e}:${t}:${n}`;
     const rs = await digestMessage(a);
     $log.error('==========' + rs)
